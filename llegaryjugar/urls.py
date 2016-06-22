f9c3d9cbe6bb32e86c928add7c7b092a4aa0f964
Loneck <leo.torrejon@gmail.com>
2016-06-22 03:19:34 +0000

    conflicts

--- llegaryjugar/urls.py
+++ llegaryjugar/urls.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-from django.conf import settings
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.conf.urls.static import static
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^ajaximage/', include('ajaximage.urls')),
-    # url(r'^inicio/', include('inicio.urls')),
-    # url(r'^recinto/', include('recinto.urls')),
-    url(r'^$', include('inicio.urls')),
-]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
@@ -35,4 +16,3 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 7e86bb1cfad9c631e66a0c8922f09bbf5deccb66
