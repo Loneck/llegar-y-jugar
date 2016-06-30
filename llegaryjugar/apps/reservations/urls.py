873be9dd347e7aee96d14859f8d17fc297d7a456
Proyecto Gutenberg <proyectogutenberg@MacBook-Pro-de-Proyecto.local>
2016-06-30 18:38:24 -0400

    wizard

--- llegaryjugar/apps/reservations/urls.py
+++ llegaryjugar/apps/reservations/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
 from django.conf.urls.static import static
 from llegaryjugar.apps.reservations.views import club_list
--- llegaryjugar/apps/reservations/views.py
+++ llegaryjugar/apps/reservations/views.py
@@ -5,44 +5,47 @@ from llegaryjugar.forms import LoginForm
 from django.contrib.auth import authenticate, login
 from formtools.wizard.views import SessionWizardView
 from django.core.mail import send_mail
+from django.template.response import TemplateResponse
 from llegaryjugar.apps.reservations.models import Club
 
 # class StepWizard(SessionWizardView):
-# 	template_name =	"inicio.html"
+#   template_name = "inicio.html"
 
-# 	def done(self, form_list, **kwargs):
-# 		form_data = process_form_data(form_list)
+#   def done(self, form_list, **kwargs):
+#       form_data = process_form_data(form_list)
 
-# 		return render_to_response('done.html', {'form_data': form_data})
+#       return render_to_response('done.html', {'form_data': form_data})
 
-# 	def process_form_data(form_list):
-# 		form_data = [form.cleaned_data for form in form_list]
+#   def process_form_data(form_list):
+#       form_data = [form.cleaned_data for form in form_list]
 
-# 		logr.debug(form_data[0]['club'])
-# 		logr.debug(form_data[1]['scheduleCourt'])
-# 		logr.debug(form_data[2]['accesories'])
-# 		logr.debug(form_data[3]['paymentMethod'])
+#       logr.debug(form_data[0]['club'])
+#       logr.debug(form_data[1]['scheduleCourt'])
+#       logr.debug(form_data[2]['accesories'])
+#       logr.debug(form_data[3]['paymentMethod'])
 
-# 		send_mail(form_data[0]['club'],
-# 				  form_data[1]['scheduleCourt'],
-# 				  form_data[2]['accesories'],
-# 				  form_data[3]['paymentMethod'],
-# 				  ['leo.torrejon@gmail.com'], fail_silently=False)
+#       send_mail(form_data[0]['club'],
+#                 form_data[1]['scheduleCourt'],
+#                 form_data[2]['accesories'],
+#                 form_data[3]['paymentMethod'],
+#                 ['leo.torrejon@gmail.com'], fail_silently=False)
 
-# 		return form_data
+#       return form_data
 
-# 	def get_form_initial(self, step):
-# 		return self.initial_dict.get(step, {'hola': 1234})
+#   def get_form_initial(self, step):
+#       return self.initial_dict.get(step, {'hola': 1234})
 
 
 def club_list(request):
-	clubs = Club.objects.all()
-	return render(request, 'inicio/inicio.html', {'clubs': clubs})
+    clubs = Club.objects.all()
+    return render(request, 'inicio/inicio.html', {'clubs': clubs})
 
 
 class ContactWizard(SessionWizardView):
+    template_name = 'wizard_form.html'
+
     def done(self, form_list, **kwargs):
-        return render_to_response('inicio/done.html', {
+        return render_to_response('done.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
     def get_form_initial(self, step):
