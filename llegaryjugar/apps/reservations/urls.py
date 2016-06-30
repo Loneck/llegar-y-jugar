56fb3e1af1408a3df3352abaf7808823a90f1ff5
Proyecto Gutenberg <proyectogutenberg@MacBook-Pro-de-Proyecto.local>
2016-06-30 16:12:19 -0400

    wizard

--- llegaryjugar/apps/reservations/urls.py
+++ llegaryjugar/apps/reservations/urls.py
@@ -3,17 +3,11 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
-from . import as_view
 from llegaryjugar.apps.reservations.views import club_list
 from llegaryjugar.apps.reservations.forms import ContactForm1, ContactForm2
 from llegaryjugar.apps.reservations.views import ContactWizard
 
 urlpatterns = [
-    url(r'^$', views.club_list),
+    # url(r'^$', views.club_list),  # Esta URL evita que se ejecute la url de abajo.
     url(r'^$', ContactWizard.as_view([ContactForm1, ContactForm2])),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
