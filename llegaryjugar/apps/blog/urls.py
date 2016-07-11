939bd748030822b33fde3c0124eb5a6d5b1fb5c9
Loneck <leo.torrejon@gmail.com>
2016-07-11 19:27:42 -0400

    add blog

--- llegaryjugar/apps/blog/urls.py
+++ llegaryjugar/apps/blog/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from .import views
 
 urlpatterns = [
-	url(r'^$', views.post_list),
-	url(r'^post/post-(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-	url(r'^post/new/$', views.post_new, name='post_new'),
-	url(r'^post/post-(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+	url(r'^blog/$', views.post_list, name='blog'),
+	url(r'^blog/post/post-(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^blog/post/new/$', views.post_new, name='post_new'),
+	url(r'^blog/post/post-(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
